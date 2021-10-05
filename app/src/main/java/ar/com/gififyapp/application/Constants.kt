df70139360f0a62a07a309abcdd8a5f393e42bdd
elfer07 <morenofernando@gmail.com>
2021-10-05 07:46:31 -0300

    Gifify App

--- app/src/main/java/ar/com/gififyapp/application/Constants.kt
+++ app/src/main/java/ar/com/gififyapp/application/Constants.kt
@@ -6,7 +6,7 @@ import ar.com.gififyapp.BuildConfig
  * Created by Fernando Moreno on 12/9/2021.
  */
 object Constants {
-    const val API_KEY = "IHfllmZarDTkWdhirWunNH0TJ5JtUYEQ"
+    const val API_KEY = "YOUR API KEY HERE"
     const val BASE_URL = "https://api.giphy.com/v1/"
     const val TABLE = "gifify_table"
     const val IMAGE_TOOLBAR = "https://media0.giphy.com/media/3o6gbbuLW76jkt8vIc/giphy.gif?cid=ecf05e475sxgc298ufim2cwxivn9ziihxd0wx1kdsucv0phg&rid=giphy.gif&ct=g"
